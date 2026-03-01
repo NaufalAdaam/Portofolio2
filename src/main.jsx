240f9575044e81375a1eceafa3f13bb3cce8a537
Naufal Adam Noorrahmat <n.adam28895@gmail.com>
2026-03-01 22:14:33 +0700

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import App from './App.jsx';
 import Navbar from './components/Navbar.jsx'; // ubah dari .tsx ke .jsx
 
 import 'remixicon/fonts/remixicon.css';
-import Footer from './components/footer.jsx';
-import PreLoader from './components/Preloader.jsx';
+import Footer from './components/Footer.jsx';
+import PreLoader from '../components/Preloader.jsx';
 
 import 'animate.css';
 import AOS from 'aos';
